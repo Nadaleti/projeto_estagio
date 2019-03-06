8be983c1a5794d3839b03408f8949b830ad265e8
Matheus Nadaleti Garcia <mat.nadaleti@hotmail.com>
2019-03-06 14:55:15 -0300

    Projeto com dependências iniciais para a web api

---
+++ backend/src/main/java/com/opussoftware/femanager/FeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.opussoftware.femanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/opussoftware/femanager/FeManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.opussoftware.femanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
