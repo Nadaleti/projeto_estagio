706415464da4fd28a24b5707228417c6f74da764
Matheus Nadaleti Garcia <mat.nadaleti@hotmail.com>
2019-03-06 15:29:33 -0300

    Criando entities

---
+++ backend/src/main/java/com/opussoftware/femanager/entities/Jogo.java
@@ -0,0 +1,54 @@
+package com.opussoftware.femanager.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Jogo {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String nome;
+	private Date ano_lancamento;
+	private String plataforma;
+
+	public Jogo() {}
+
+	public Date getAno_lancamento() {
+		return ano_lancamento;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setAno_lancamento(Date ano_lancamento) {
+		this.ano_lancamento = ano_lancamento;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+}
