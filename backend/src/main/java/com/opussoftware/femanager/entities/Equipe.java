67367f06ddb2a80d08a0e1eabb69018c12a5d016
BrunoMorii <brunomorii.1998@gmail.com>
2019-03-08 17:56:55 -0300

    Adicionando dados e tentativa de corrigir inicialização DML

--- backend/src/main/java/com/opussoftware/femanager/entities/Equipe.java
+++ backend/src/main/java/com/opussoftware/femanager/entities/Equipe.java
@@ -1,6 +1,5 @@
 package com.opussoftware.femanager.entities;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
