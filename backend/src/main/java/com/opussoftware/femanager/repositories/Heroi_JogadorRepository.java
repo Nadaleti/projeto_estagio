f4ad6b93fdc628980ad00aa82a089a76888502d8
Matheus Nadaleti Garcia <mat.nadaleti@hotmail.com>
2019-03-15 13:20:17 -0300

    Adição dos métodos find

--- backend/src/main/java/com/opussoftware/femanager/repositories/Heroi_JogadorRepository.java
+++ backend/src/main/java/com/opussoftware/femanager/repositories/Heroi_JogadorRepository.java
@@ -1,9 +1,24 @@
 package com.opussoftware.femanager.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.opussoftware.femanager.entities.Heroi;
 import com.opussoftware.femanager.entities.Heroi_jogador;
 
 public interface Heroi_JogadorRepository extends JpaRepository<Heroi_jogador, Long>{
+
+	List<Heroi_jogador> findByNomeContaining(String string, Sort sort);
+
+	List<Heroi_jogador> findByClasseContaining(String string, Sort sort);
+
+	List<Heroi_jogador> findByMovimentacaoContaining(String string, Sort sort);
+	
+	@Query("SELECT h FROM Heroi h WHERE lower(h.nome) like :nome" + " AND lower(h.classe) like :classe"
+			+ " AND lower(h.movimentacao) like :movimentacao")
+	public List<Heroi_jogador> findByNomeAndClasseAndMovimentacao(String nome, String classe, String movimentacao,Sort sort);
 	
 }
